5efcfa8601e8b93a7834e3f78856d7b9b173b05d
Ubuntu <ubuntu@ip-172-31-30-44.ec2.internal>
2022-04-20 14:38:30 +0000

    Testing jenkins automation

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var handleRequest = function(request, response) {
   response.write("DevOps Coursework 2! | Running on: ");
   response.write(host);
   response.end(" | v=0\n");
-  console.log("Running On:" ,host, "| Total Requests:", ++requests,"| App Uptime:", (new Date() - startTime)/1000 , "seconds", "| Log Time:",new Date());}
+  console.log("Running  On:" ,host, "| Total Requests:", ++requests,"| App Uptime:", (new Date() - startTime)/1000 , "seconds", "| Log Time:",new Date());}
 
 var www = http.createServer(handleRequest);
 www.listen(8080,function () {
